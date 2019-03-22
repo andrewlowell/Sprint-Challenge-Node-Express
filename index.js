3c2a16fff2146cd27b910606518d5c918df46ec6
andrewlowell <andrewlowellr@gmail.com>
2019-03-22 09:41:56 -0600

    Initialize project and finish index.js and project router.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // play this: https://www.youtube.com/watch?v=d-diB65scQU
 
-// code away!
+const express = require('express');
+const projects = require('./projects/projectRouter.js');
+// const actions = require('./actions/actionRouter.js');
+
+const app = express();
+app.use(express.json());
+app.use('/projects', projects);
+// app.use('/actions', actions);
+
+app.get('/', (req, res) => {
+  res.send("Jake, why are you here? What confluence of powers and events conspired to bring you into existence at this very time?");
+})
+
+const port = process.env.PORT || 5555;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
